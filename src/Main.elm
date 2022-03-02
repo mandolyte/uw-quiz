2b347f280ef8a3e529d4821871d57ceb92013951
mandolyte <cecil.new@gmail.com>
2022-03-02 09:27:00 -0500

    WIP mulitple questions 1

--- src/Main.elm
+++ src/Main.elm
@@ -9,7 +9,7 @@ import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
 import Element.Region as Region
-
+import Html exposing (..)
 
 white =
     Element.rgb 1 1 1
@@ -38,27 +38,44 @@ main =
         , update = update
         }
 
+type AnswerId = AnswerId String
+type QuestionId = QuestionId String
+
 type alias Answer =
-    { id : String
-    , text : String
+    { id : AnswerId
+    , description : String
+    }
+
+type alias Question =
+    { id: QuestionId
+    , description: String
+    , selectedAnswer: AnswerId
+    , correctAnswer: AnswerId
+    , answers: List Answer
     }
 
 type alias Model =
-  { title: String
-  , currentAnswer: String
-  , answers: List Answer
-  , question: String
-  }
+    { title: String
+    , currentQuestion: QuestionId
+    , questions: List Question
+    }
 
 init : Model
 init = 
-    Model "This the title for the quiz" "" 
-        [ Answer "A1" "This is the first answer"
-        , Answer "A2" "This is the second answer"
-        , Answer "A3" "All of the above"
-        , Answer "A4" "None of the above"
+    Model "This the title for the quiz" "q1"
+    [ Question "q1" "This is question 1?" "" "a1"
+        [ Answer "a1" "This is answer 1."
+        , Answer "a2" "This is answer 2."
+        , Answer "a3" "All of above."
+        , Answer "a4" "None of above."
         ]
-        "This is a question?"
+    , Question "q2" "This is question 2?" "" "a8"
+        [ Answer "a5" "This is answer 1."
+        , Answer "a6" "This is answer 2."
+        , Answer "a7" "All of above."
+        , Answer "a8" "None of above."
+        ]
+    ]
 
 type Msg
     = Update Model
@@ -71,7 +88,9 @@ update msg _ =
 
 
 makeInput : Answer -> Input.Option String Msg
-makeInput answer = Input.option answer.id (Element.text answer.text)
+makeInput answer = Input.option answer.id (Element.text answer.description)
+
+isCurrentQuestion question = if question.id == model.currentQuestion then True else False
 
 view model =
     Element.layout
@@ -91,17 +110,8 @@ view model =
                 , alignLeft
                 , Font.size 36
                 ]
-                (text model.title)
-            , Input.radio
-                [ spacing 12
-                , padding 10
-                , Background.color grey
-                ]
-                { selected = Just model.currentAnswer
-                , onChange = \new -> Update { model | currentAnswer = new }
-                , label = Input.labelAbove [ Font.size 20, paddingXY 0 12 ] (text model.question)
-                , options = List.map makeInput model.answers 
-                }
+                (Element.text model.title)
+            , viewQuestion List.filter isCurrentQuestion model.questions -- how do I specify which question to show
             , Input.button
                 [ Background.color blue
                 , Font.color white
@@ -115,3 +125,17 @@ view model =
                 , label = Element.text "Continue"
                 }
             ]
+
+viewQuestion : Question -> Element Msg
+viewQuestion question = 
+    Input.radio
+        [ spacing 12
+        , padding 10
+        , Background.color grey
+        ]
+        { selected = Just question.selectedAnswer
+        , onChange = \new -> Update { question | selectedAnswer = new }
+        , label = Input.labelAbove [ Font.size 20, paddingXY 0 12 ] (Element.text question.description)
+        , options = List.map makeInput question.answers 
+        }
+        
