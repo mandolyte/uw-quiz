571be70b78eb7d3681068b252873c1529b5b4326
mandolyte <cecil.new@gmail.com>
2022-03-02 19:48:37 -0500

    WIP: continue button working

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,7 @@ import Element.Font as Font
 import Element.Input as Input
 import Element.Region as Region
 import Html exposing (..)
+import List.Extra exposing (getAt)
 
 white =
     Element.rgb 1 1 1
@@ -40,6 +41,8 @@ main =
 
 type AnswerId = AnswerId String
 type QuestionId = QuestionId String
+answerIdAsString (AnswerId idString) = idString
+questionIdAsString (QuestionId idString) = idString
 
 type alias Answer =
     { id : AnswerId
@@ -58,39 +61,56 @@ type alias Model =
     { title: String
     , currentQuestion: QuestionId
     , questions: List Question
+    , scratch: String
     }
 
 init : Model
 init = 
-    Model "This the title for the quiz" "q1"
-    [ Question "q1" "This is question 1?" "" "a1"
-        [ Answer "a1" "This is answer 1."
-        , Answer "a2" "This is answer 2."
-        , Answer "a3" "All of above."
-        , Answer "a4" "None of above."
+    Model "This the title for the quiz" (QuestionId "q1")
+    [ Question (QuestionId "q1") "This is question 1?" (AnswerId "") (AnswerId "a1")
+        [ Answer (AnswerId "a1") "This is answer 1."
+        , Answer (AnswerId "a2") "This is answer 2."
+        , Answer (AnswerId "a3") "All of above."
+        , Answer (AnswerId "a4") "None of above."
         ]
-    , Question "q2" "This is question 2?" "" "a8"
-        [ Answer "a5" "This is answer 1."
-        , Answer "a6" "This is answer 2."
-        , Answer "a7" "All of above."
-        , Answer "a8" "None of above."
+    , Question (QuestionId "q2") "This is question 2?" (AnswerId "") (AnswerId "a8")
+        [ Answer (AnswerId "a5") "This is q2, answer 1."
+        , Answer (AnswerId "a6") "This is q2, answer 2."
+        , Answer (AnswerId "a7") "All of above."
+        , Answer (AnswerId "a8") "None of above."
         ]
     ]
+    ""
+
+nextEntry : Model -> Model
+nextEntry model =
+    if model.currentQuestion == QuestionId "q2" then
+        {model | currentQuestion = QuestionId "q1"}
+    else
+        { model | currentQuestion = QuestionId "q2" }
+
+            -- if model.current < List.length model.entries then
+            --     ( model |> Model.nextEntry, Cmd.none )
+
+            -- else
+            --     ( model, Cmd.none )
+
+
 
 type Msg
     = Update Model
+    | NextEntry
 
-
-update msg _ =
+update msg model =
     case Debug.log "msg" msg of
         Update new ->
             new
 
+        NextEntry ->
+            ( model |> nextEntry )
 
 makeInput : Answer -> Input.Option String Msg
-makeInput answer = Input.option answer.id (Element.text answer.description)
-
-isCurrentQuestion question = if question.id == model.currentQuestion then True else False
+makeInput answer = Input.option (answerIdAsString answer.id) (Element.text answer.description)
 
 view model =
     Element.layout
@@ -111,7 +131,7 @@ view model =
                 , Font.size 36
                 ]
                 (Element.text model.title)
-            , viewQuestion List.filter isCurrentQuestion model.questions -- how do I specify which question to show?
+            , viewQuestion model
             , Input.button
                 [ Background.color blue
                 , Font.color white
@@ -121,21 +141,55 @@ view model =
 
                 -- , width fill
                 ]
-                { onPress = Nothing
+                { onPress = Just NextEntry 
                 , label = Element.text "Continue"
                 }
             ]
 
-viewQuestion : Question -> Element Msg
-viewQuestion question = 
+viewQuestion : Model -> Element Msg
+viewQuestion model = 
+    let
+        curQuestionList =  List.filter (\question -> question.id == model.currentQuestion) model.questions
+        curQuestion = getAt 0 curQuestionList
+        -- curQuestion2 = find (\q -> q.id == model.currentQuestion ) model.questions
+        justCurQuestion = 
+            case curQuestion of
+                Just x -> x
+                Nothing -> { answers = []
+                            , correctAnswer = AnswerId("a0")
+                            , description = "String"
+                            , id = QuestionId("q0")
+                            , selectedAnswer = AnswerId("a0")
+                            }
+        curQuestionIdAsString = 
+            case curQuestion of
+                Nothing -> ""
+                Just q -> questionIdAsString q.id
+        curQuestionDescription = 
+            case curQuestion of
+                Nothing -> ""
+                Just q -> q.description
+        curQuestionAnswers =
+            case curQuestion of
+                Nothing -> []
+                Just q -> q.answers
+    in
     Input.radio
         [ spacing 12
         , padding 10
         , Background.color grey
         ]
-        { selected = Just question.selectedAnswer
-        , onChange = \new -> Update { question | selectedAnswer = new }
-        , label = Input.labelAbove [ Font.size 20, paddingXY 0 12 ] (Element.text question.description)
-        , options = List.map makeInput question.answers 
+        { selected = Just curQuestionIdAsString
+        , onChange = \new -> Update { model | scratch = "" }
+        , label = Input.labelAbove [ Font.size 20, paddingXY 0 12 ] (Element.text curQuestionDescription)
+        , options = List.map makeInput curQuestionAnswers 
         }
         
+
+
+{-
+
+            , viewQuestion List.filter (\question -> question.id == model.currentQuestion) model.questions
+
+                { onPress = \new -> Update { model | currentQuestion = QuestionId("q2") }
+-}
